0179f6ebac638feed4e7cd84bb844e5555dd4ee5
Ferid HELALI <helaliferid@gmail.com>
2024-06-11 02:40:30 +0100

    to fix sqlite3 driver integration 2

--- src/main/typeorm.config.ts
+++ src/main/typeorm.config.ts
@@ -1,20 +1,20 @@
-
-import { DataSource } from 'typeorm';
+import { SqliteDriver } from 'typeorm/driver/sqlite/SqliteDriver';
+import { join } from 'path';
 import { ActualMonthSalary } from './db/entities/ActualMonthSalary'; 
 import { Affair } from './db/entities/Affair'; 
 import { AgreementApplicationPeriods } from './db/entities/AgreementApplicationPeriods'; 
 import { SalaryTable } from './db/entities/SalaryTable'; 
 import { SectorialJointAgreement } from './db/entities/SectorialJointAgreement'; 
 import { SalaryTableCell } from './db/entities/SalaryTableCell';
-import { SqliteDriver } from 'typeorm/driver/sqlite/SqliteDriver';
+import { DataSource } from 'typeorm';
 
 console.log('NODE_ENV:', process.env.NODE_ENV);
 console.log('sqlite3 path:', require.resolve('sqlite3'));
 
 const AppDataSource = new DataSource({
     type: 'sqlite',
-    database: '../../payroll.db',
-    entities: [ActualMonthSalary, Affair,SectorialJointAgreement,AgreementApplicationPeriods,SalaryTableCell,SalaryTable], // Add all your entity classes here
+    database: join(__dirname, '..', 'payroll.db'),
+    entities: [ActualMonthSalary, Affair, SectorialJointAgreement, AgreementApplicationPeriods, SalaryTableCell, SalaryTable], 
     synchronize: true,
 });
 
