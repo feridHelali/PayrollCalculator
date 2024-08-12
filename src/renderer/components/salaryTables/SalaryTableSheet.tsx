ec53bd34bbb82c0bcb643c0b4cea78e5be651494
Ferid HELALI <helaliferid@gmail.com>
2024-08-12 18:02:16 +0100

    more style like Excel sheet

--- src/renderer/components/salaryTables/SalaryTableSheet.tsx
+++ src/renderer/components/salaryTables/SalaryTableSheet.tsx
@@ -1,32 +1,13 @@
-import React, { FC, useState } from 'react';
-import { Box, Button, Table, Tbody, Td, Th, Thead, Tr, VStack } from '@chakra-ui/react';
-//import { FaSave } from 'react-icons/fa';
-import { ProfessionalCategory, ProfessionalDegree } from '../../../types/salaryTableProps';
+import React, { FC } from 'react';
+import { Box, Table, Tbody, Td, Th, Thead, Tr, VStack } from '@chakra-ui/react';
+// import { FaSave } from 'react-icons/fa';
 import { labels } from '../../arabic.labels';
 import { useSalaryTableContext } from './contexts/SalaryTableContext';
-import  './SalaryTableSheet.module.css'
-
-
-interface Header {
-  key: string;
-  degree: number;
-  ageOfWork: number;
-}
-
-interface Category {
-  key: string;
-  label: string;
-}
-
-
-type RowData = {
-  category: string;
-  [key: string]: number | string;
-};
+import './SalaryTableSheet.css'; // Ensure this path is correct
 
 const SalaryTableSheet: FC = () => {
-  
   const { categories, headers, salaryTableCells, setSalaryTableCells } = useSalaryTableContext();
+
   const handleSalaryChange = (categoryKey: string, degreeKey: string, value: string) => {
     setSalaryTableCells((prevSalaryTableCells) => ({
       ...prevSalaryTableCells,
@@ -38,29 +19,30 @@ const SalaryTableSheet: FC = () => {
   };
 
   return (
-    <Box className='salary-table-sheet__container'>
-      <Table  className='salary-table-sheet__table'>
-        <Thead className='salary-table-sheet__header'>
+    <Box className="salary-table-sheet__container">
+      <Table className="salary-table-sheet__table">
+        <Thead className="salary-table-sheet__header">
           <Tr>
-            <Th className='salary-table-sheet__header-cell'>{labels.category}</Th>
+            <Th className="salary-table-sheet__header-cell">{labels.category}</Th>
             {headers.map((header) => (
-              <Th key={header.key}  className='salary-table-sheet__header-cell'>
-                {labels.degree} {header.degree} <br />{labels.ageOfWork}: {header.ageOfWork}
+              <Th key={header.key} className="salary-table-sheet__header-cell">
+                {labels.degree} {header.degree} <br />
+                {labels.ageOfWork}: {header.ageOfWork}
               </Th>
             ))}
           </Tr>
         </Thead>
-        <Tbody className='salary-table-sheet__body'>
+        <Tbody className="salary-table-sheet__body">
           {categories.map((category) => (
-            <Tr key={category.key} className='salary-table-sheet__body-row'>
-              <Td className='salary-table-sheet__body-row-cell'>{category.label}</Td>
+            <Tr key={category.key} className="salary-table-sheet__body-row">
+              <Td className="salary-table-sheet__body-cell">{category.label}</Td>
               {headers.map((header) => (
-                <Td key={header.key} className='salary-table-sheet__body-row-cell'>
+                <Td key={header.key} className="salary-table-sheet__body-cell">
                   <input
                     type="number"
+                    className="salary-table-sheet__body-input"
                     value={salaryTableCells[category.key]?.[header.key] || ''}
                     onChange={(e) => handleSalaryChange(category.key, header.key, e.target.value)}
-                    style={{ width: '80%' }}
                   />
                 </Td>
               ))}
